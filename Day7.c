1251fd48a1eb170e619b5030d08875e06b1a7ce2
Nia2111 <niayadav13@gmail.com>
2026-02-10 09:23:14 +0530

    Create Day7.c

---
+++ Day7.c
@@ -0,0 +1,62 @@
+Problem1: Write a recursive function fib(n) to compute the n-th Fibonacci number where fib(0)=0 and fib(1)=1.
+Input:
+- Single integer n
+Output:
+- Print the n-th Fibonacci number
+Example:
+Input:
+6
+Output:
+8
+Explanation: Sequence: 0,1,1,2,3,5,8 at positions 0,1,2,3,4,5,6
+
+Answer:
+#include <stdio.h>
+
+int fib(int n) {
+    if (n <= 1) return n;
+    
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", fib(n));
+    return 0;
+}
+
+Problem2:The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+Example 1:
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+Constraints:
+0 <= n <= 30
+
+Answer:
+int fib(int n) {
+        if (n <= 1) return n;
+        
+        int prev2 = 0, prev1 = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
+    }
+}
